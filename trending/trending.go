33da1ab36f355ec31d770288ee263fc8aa4dd87f
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-12-05 23:16:34 +0530

    trending/trending.go : Added some documentation

--- trending/trending.go
+++ trending/trending.go
@@ -1,7 +1,7 @@
 package trending
 
 import (
-	"fmt"
+	"errors"
 	"log"
 	"strconv"
 	"strings"
@@ -19,30 +19,44 @@ const (
 )
 
 type Trending struct {
-	lang  string
-	Repos []Repo
-	since string
+	Repos []Repo // Trending repos
+	lang  string // If lang is set, Repos are only of the particular language. (Default is all)
+	since string // The timespan of the trending repos (Monthly, Weekly, Daily). (Default is daily)
 }
 
 type Repo struct {
-	URL   string
-	Owner string
-	Name  string
-	Stars int
+	URL   string // URL repo, eg : github.com/kalbhor/gotrending
+	Owner string // Owner of repo
+	Name  string // Name of repo
+	Stars int    // Number of stars on repo
 }
 
+// Returns a Trending type. Sets default values for lang and since
 func NewTrending() *Trending {
 	t := &Trending{lang: "", since: Today}
 	return t
 }
 
-func (t *Trending) SetLang(lang string) {
+/* To do : check if lang is valid, return error */
+
+// Sets the lang on a Trending type
+func (t *Trending) SetLang(lang string) error {
 	t.lang = lang
-}
-func (t *Trending) SetSince(since string) {
-	t.since = since
+	return nil
 }
 
+// Sets the Since on a Trending type
+func (t *Trending) SetSince(since string) error {
+	if since != Today || since != Week || since != Month {
+		t.since = Today
+		return errors.New("Invalid value for Trending.Since")
+	}
+	t.since = since
+	return nil
+}
+
+// Run scrapes github.com/trending to get trending repos.
+// Returns a slice of repo with trending repos.
 func (t *Trending) Get() []Repo {
 	var repos []Repo
 	var repo Repo
@@ -55,7 +69,7 @@ func (t *Trending) Get() []Repo {
 	}
 
 	doc.Find("ol.repo-list li").Each(func(i int, s *goquery.Selection) {
-		repoURL, err := getRepoPath(s)
+		repoPath, err := getRepoPath(s) // Eg : kalbhor/gotrending
 		if err != nil {
 			log.Printf("%v\n", err)
 		}
@@ -65,16 +79,16 @@ func (t *Trending) Get() []Repo {
 			log.Printf("%v\n", err)
 		}
 
-		pathSplit := strings.Split(repoURL, "/")
+		pathSplit := strings.Split(repoPath, "/") // Split repoPath into the owner and name of repo
 
-		repo.URL = MainEndpoint + repoURL
+		repo.URL = MainEndpoint + repoURL // Eg : github.com/ + kalbhor/gotrending
 		repo.Owner = pathSplit[0]
 		repo.Name = pathSplit[1]
-		repo.Stars, err = strconv.Atoi(stars)
+		repo.Stars, err = strconv.Atoi(stars) // Convert stars to an int
 		if err != nil {
 			log.Printf("%v\n", err)
 		}
-		repos = append(repos, repo)
+		repos = append(repos, repo) // Append trending repo to repos
 	})
 
 	t.Repos = repos
@@ -82,6 +96,9 @@ func (t *Trending) Get() []Repo {
 
 }
 
+/* To do : check if path is valid, return error */
+
+// Parses html to get the repo's path
 func getRepoPath(s *goquery.Selection) (string, error) {
 	repoPath := s.Find("h3 a").Text()
 	repoPath = strings.Replace(repoPath, "\n", "", -1)
@@ -90,6 +107,9 @@ func getRepoPath(s *goquery.Selection) (string, error) {
 	return repoPath, nil
 }
 
+/* To do : check if stars is valid, return error */
+
+// Parses html to get number of stars
 func getRepoStars(s *goquery.Selection) (string, error) {
 	stars := s.Find("div.f6 a").First().Text()
 	stars = strings.Replace(stars, "\n", "", -1)
@@ -97,11 +117,4 @@ func getRepoStars(s *goquery.Selection) (string, error) {
 	stars = strings.Replace(stars, ",", "", -1)
 
 	return stars, nil
-}
-
-func (r *Repo) Print() {
-	fmt.Println("URL : ", r.URL)
-	fmt.Println("Owner : ", r.Owner)
-	fmt.Println("Repo Name : ", r.Name)
-	fmt.Println("Stars : ", r.Stars)
 }
